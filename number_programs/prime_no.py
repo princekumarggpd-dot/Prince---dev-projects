5572d5c6ffb22d59ac9abe86d7fe46606d0d0819
princekumarggpd-dot <princekumarggpd@gmail.com>
2026-01-01 13:34:34 +0530

    Program to check if a number entered by user is prime number or not

--- number_programs/prime_no.py
+++ number_programs/prime_no.py
@@ -1,1 +1,15 @@
+#Program to check a number is prime number or not
+n = int(input("Enter the no"))
+if n <= 1:
+    print("false")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("Its not Prime")
+            break
+    else:
+        print("Prime")
+
+
+
 
