d8a3b05004744e20c03806423925bda965fd8848
princekumarggpd-dot <princekumarggpd@gmail.com>
2026-01-01 13:45:48 +0530

    Implement palindrome number checker
    Added a palindrome check using two methods.

--- number_programs/Palindrome_number.py
+++ number_programs/Palindrome_number.py
@@ -1,1 +1,17 @@
+#Check for Palindrome no
+a = input("Enter the 1st no:")
+if a[::-1]==a:
+    print("Its a Palindrome no")
+else:
+    print("Not a Palindrome no")
+#2nd Method
+r = ""
+e = input("Enter no"))
+for i in e:
+  r+=i
+if r == e:
+  print("It's a Palindrome no")
+else:
+  return False
+
 
